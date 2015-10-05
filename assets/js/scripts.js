d199d069274f77553fea0f82d47ed7b4ce8fdc6f
Ivana <Setiawan@Ivana.local>
2015-10-05 16:58:05 +0200

    Minify

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -107,7 +107,7 @@ $(document).ready(function(){
 
 $(window).load(function() {
 
-	$('#portfolioSlider').flexslider({
+	$('#imageGallery').flexslider({
 		animation: "slide",
 		directionNav: false,
 		controlNav: true,
